ca08caf031de72db07c04af244e0338e3ba9bfae
Charles Kanakan <50149696+CKanakan@users.noreply.github.com>
2019-05-02 08:42:03 -0400

    Add files via upload

---
+++ EnglishToMetric.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishToMetricToEnglish
+{
+    class EnglishToMetric
+    {
+        private int miles;
+        private int yards;
+        private int feet;
+        private double inches;
+        private int kilometer;
+        private int meters;
+        private double centimeters;
+
+        public EnglishToMetric()
+        {
+            //the attributes will be initialized here 
+        }
+
+        public EnglishToMetric(String theMiles, String theYards, String feet, String theInches)
+        {
+            convertEnglishToInches();
+            convertInchesToCentimeters();
+            convertCentimetersToMetersAndKilometers();
+        }
+
+        //private bool setMiles(String theMiles)
+        //{
+        //This will set the Miles
+        //}
+
+        public void convert()
+        {
+            convertEnglishToInches();
+        }
+
+        private void convertEnglishToInches()
+        {
+            convertMilesToYards();
+            convertYardsToFeet();
+            convertFeetToInches();
+        }
+
+        private void convertMilesToYards()
+        {
+            yards += miles * 1760;
+        }
+        private void convertYardsToFeet()
+        {
+            feet = yards * 3;
+        }
+
+        private void convertFeetToInches()
+        {
+            inches = feet * 12;
+        }
+        private void convertInchesToCentimeters()
+        {
+            centimeters = inches * 0.393701;
+        }
+        private void extractKilometerFromCentimeter()
+        {
+            //will take centimeters and turn it into
+            //Kilometers
+            kilometer = (int)centimeters / 100000;
+            centimeters = centimeters % 100000;
+        }
+
+        public String getKilometers()
+        {
+            return kilometer.ToString();
+        }
+
+        private void extractMetersFromCentimeter()
+        {
+            //will take centimeters and turn it into
+            //Meters
+            meters = (int)centimeters / 100;
+            centimeters = centimeters % 100;
+        }
+
+        public String getMeters()
+        {
+            return meters.ToString();
+        }
+
+        private void convertCentimetersToMetersAndKilometers()
+        {
+            extractMetersFromCentimeter();
+            extractKilometerFromCentimeter();
+        }
+
+
+
+    }
+}
---
+++ MetricToEnglish.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishToMetricToEnglish
+{
+    class MetricToEnglish
+    {
+        private int miles;
+        private int yards;
+        private int feet;
+        private double inches;
+        private int kilometer;
+        private int meters;
+        private double centimeters;
+
+        public MetricToEnglish()
+        {
+            //the attributes can be initialized here 
+        }
+
+        public MetricToEnglish(String theKilometer, String theMeters, String theCentimeters)
+        {
+            //the attributes will not have to be set if this called
+            convertMetricToCentimeters();
+        }
+
+        public void convert()//used if the default constructor is used 
+        {
+            convertMetricToCentimeters();
+        }
+        private void convertMetricToCentimeters()
+        {
+            convertCentimeterToMeters();
+            convertMeterToKilometers();
+        }
+
+        private void convertCentimeterToMeters()
+        {
+            meters = (int)centimeters + 100;
+        }
+        private void convertMeterToKilometers()
+        {
+            kilometer = meters + 1000;
+        }
+        private void convertCentimetersToInches()
+        {
+            inches = centimeters * 2.54;
+        }
+        public String getInches()
+        {
+            return inches.ToString();
+        }
+        private void extractMilesFromInches()
+        {
+            miles = (int)inches + 63360;
+            inches = inches % 63360;
+        }
+
+        public String getMiles()
+        {
+            return miles.ToString();
+        }
+        private void extractYardsFromInches()
+        {
+            yards = (int)inches * 36;
+            inches = inches % 63360;
+        }
+        public String getYards()
+        {
+            return yards.ToString();
+        }
+        private void extractFeetFromInches()
+        {
+            yards = (int)inches * 12;
+            inches = inches % 12;
+        }
+        public String getFeet()
+        {
+            return feet.ToString();
+        }
+
+    }
+}
